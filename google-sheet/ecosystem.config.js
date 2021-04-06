d987b61b58b260d51d939c7d8006d44fbab7e661
hoangtd15 <47965924+hoangtd15@users.noreply.github.com>
2021-04-06 11:23:40 +0700

    Update ecosystem.config.js

--- google-sheet/ecosystem.config.js
+++ google-sheet/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [{
-      name: "app",
-      script: "./app.js",
+      name: "index",
+      script: "./index.js",
       env: {
         NODE_ENV: "development",
       },
